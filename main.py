ddeab774c6071432715461ed3d33aa088399edc6
UbiDevs <55ubidevs@gmail.com>
2025-01-22 19:23:04 +0600

    Main Fix

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ pg.init()
 from classes.classGame import Game
 
 if __name__ == '__main__':
-    
-    game = Game()
-    game.runGame()
-    
-    pg.quit()
-    exit()
+    try:
+        game = Game()
+        game.runGame()
+    finally:
+        pg.quit()
+        exit()
