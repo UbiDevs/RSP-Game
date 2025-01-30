b9c83fcd2dcc1f3367a4c36135de96ca1130d4ca
UbiDevs <55ubidevs@gmail.com>
2025-01-30 13:22:12 +0600

    Update

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pygame as pg
 from sys import exit
 
+from sys import stdout
+
+from loguru import logger
+
+logger.add(stdout,
+           format="{time:DD.MM.YYYY HH:mm:ss} | {level} | {file}:{line} | {message}",
+           level="DEBUG")
+
 pg.init()
 
 from classes.classGame import Game
 
+@logger.catch
+def main():
+    game = Game()
+    game.runGame()
+
 if __name__ == '__main__':
     try:
-        game = Game()
-        game.runGame()
+        main()
     finally:
         pg.quit()
         exit()
